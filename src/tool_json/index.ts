ea0f3d6a551abb96171fcfa8007d562a17258d89
Jason St.Cyr <jason@morrigu.ai>
2024-04-17 10:48:16 -0700

    added json files and update broke build

--- src/tool_json/index.ts
+++ src/tool_json/index.ts
@@ -1,8 +1,6 @@
 import display_chart from './frontend/display_chart';
 import { quickbooks_query } from './quickbooks_query';
 import quickbooks_update from './backend/quickbooks_update';
-import github_api_integration from './backend/github_api_integration';
-import zendesk_support_integration from './backend/zendesK-support_integration';
 
 export const frontend_tools = [display_chart];
 export const backend_tools = [
